84acaee27cd713f4acd1b3277ae8189babf1076f
tmxhsk99 <tmxhsk99@naver.com>
2024-08-08 09:14:05 +0900

    init commit

---
+++ src/main/java/com/kjh/ollama/langserve/LangserveApplication.java
@@ -0,0 +1,13 @@
+package com.kjh.ollama.langserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LangserveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kjh/ollama/langserve/LangserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjh.ollama.langserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
