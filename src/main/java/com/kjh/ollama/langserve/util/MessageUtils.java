4dcbc2d2e56e28765601195a778564bb49f5da2a
tmxhsk99 <tmxhsk99@naver.com>
2024-09-15 15:45:49 +0900

    chore : 유틸명 변경

--- src/main/java/com/kjh/ollama/langserve/util/MessageUtils.java
+++ src/main/java/com/kjh/ollama/langserve/util/MessageUtils.java
@@ -1,6 +1,6 @@
 package com.kjh.ollama.langserve.util;
 
-public class MessageUtil {
+public class MessageUtils {
     public static String toErrorMessageFormat(Exception e) {
         return String.format(
                 "\nErrorMessage : %s\n StackTraceElement : %s", e.getMessage(), getStackTraceMsg(e.getStackTrace())
